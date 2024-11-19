c566628d6e29ddfc3b7ab59cee9370e62eca94d4
Alex <muhammaddafa6279@gmail.com>
2024-11-19 07:04:37 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Sesuaikan path dengan struktur proyek Anda
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
